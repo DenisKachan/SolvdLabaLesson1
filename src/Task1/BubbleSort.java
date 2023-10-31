b7a2a5da7b912a10073a971b0cd500e258710fd9
Denis Kachan <den.kachan.98@mail.ru>
2023-10-31 21:29:17 +0300

    variable im method added

--- src/Task1/BubbleSort.java
+++ src/Task1/BubbleSort.java
@@ -3,14 +3,13 @@ package Task1;
 public class BubbleSort {
 
     private boolean isSorted = false;
-    private int temp;
 
     public int[] bubbleSortArray(int[] array) {
         while (!isSorted) {
             isSorted = true;
             for (int i = 0; i < array.length - 1; i++) {
                 if (array[i] > array[i + 1]) {
-                    temp = array[i];
+                    int temp = array[i];
                     array[i] = array[i + 1];
                     array[i + 1] = temp;
                     isSorted = false;
